d81939ea883449d8b2f694faf1982aa383b36837
Anilforextra <anilforextra@gmail.com>
2025-02-11 22:29:30 +0545

    ImageViewer: Object Oriented Approach.

--- ImageViewer.c
+++ ImageViewer.c
@@ -2,60 +2,83 @@
 
 #include "raylib.h"
 
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 600
+#define SCREEN_WIDTH GetScreenWidth()
+#define SCREEN_HEIGHT GetScreenHeight()
+
+struct ImageViewer{
+    Texture2D imageTexture;
+    Vector2 imagePos;
+    Rectangle closeButtonRec;
+    bool dragging;
+    Vector2 dragOffset;
+};
+
+void ImageViewerInit(struct ImageViewer *viewer, const char *imagePath) {
+    viewer->imageTexture = LoadTexture(imagePath);
+    viewer->imagePos = (Vector2){ (SCREEN_WIDTH - viewer->imageTexture.width) / 2, 
+                                  (SCREEN_HEIGHT - viewer->imageTexture.height) / 2 };
+    viewer->closeButtonRec = (Rectangle){ viewer->imagePos.x + viewer->imageTexture.width - 20, viewer->imagePos.y, 20, 20 };
+    viewer->dragging = false;
+    viewer->dragOffset = (Vector2){0, 0};
+}
+
+void ImageViewerFree(struct ImageViewer *viewer) {
+    UnloadTexture(viewer->imageTexture);
+}
+
+void ImageViewerUpdate(struct ImageViewer *viewer) {
+    Vector2 mouse = GetMousePosition();
+
+    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+        if (CheckCollisionPointRec(mouse, (Rectangle){ viewer->imagePos.x, viewer->imagePos.y, viewer->imageTexture.width, viewer->imageTexture.height })) {
+            viewer->dragging = true;
+            viewer->dragOffset.x = mouse.x - viewer->imagePos.x;
+            viewer->dragOffset.y = mouse.y - viewer->imagePos.y;
+        } else if (CheckCollisionPointRec(mouse, viewer->closeButtonRec)) {
+            // TODO
+        }
+    }
+    
+    if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
+        viewer->dragging = false;
+    }
+
+    if (viewer->dragging) {
+        viewer->imagePos.x = mouse.x - viewer->dragOffset.x;
+        viewer->imagePos.y = mouse.y - viewer->dragOffset.y;
+        viewer->closeButtonRec.x = viewer->imagePos.x + viewer->imageTexture.width - 20;
+        viewer->closeButtonRec.y = viewer->imagePos.y;
+    }
+}
+
+void ImageViewerRender(struct ImageViewer *viewer) {
+    DrawTextureEx(viewer->imageTexture, viewer->imagePos, 0.0f, 1.0f, WHITE);
+    DrawLineEx((Vector2){viewer->closeButtonRec.x, viewer->closeButtonRec.y}, (Vector2){viewer->closeButtonRec.x + viewer->closeButtonRec.width, viewer->closeButtonRec.y + viewer->closeButtonRec.height}, 2, RED);
+    DrawLineEx((Vector2){viewer->closeButtonRec.x + viewer->closeButtonRec.width, viewer->closeButtonRec.y}, (Vector2){viewer->closeButtonRec.x, viewer->closeButtonRec.y + viewer->closeButtonRec.height}, 2, RED);
+}
+
 
 int main(void) {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Draggable Image with Close Button");
+    ToggleFullscreen();
 
-    Texture2D imageTexture = LoadTexture("my_amazing_texture_painting.png");
-    Vector2 imagePos = { (SCREEN_WIDTH - imageTexture.width) / 2, 
-                         (SCREEN_HEIGHT - imageTexture.height) / 2 };
-    
-    Rectangle closeButton = { imagePos.x + imageTexture.width - 20, imagePos.y, 20, 20 };
-    bool dragging = false;
-    Vector2 dragOffset = {0, 0};
+    struct ImageViewer viewer;
+    ImageViewerInit(&viewer, "my_amazing_texture_painting.png");
 
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
-        Vector2 mouse = GetMousePosition();
-
-        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-            if (CheckCollisionPointRec(mouse, (Rectangle){ imagePos.x, imagePos.y, imageTexture.width, imageTexture.height })) {
-                dragging = true;
-                dragOffset.x = mouse.x - imagePos.x;
-                dragOffset.y = mouse.y - imagePos.y;
-            } else if (CheckCollisionPointRec(mouse, closeButton)) {
-                // TODO
-            }
-        }
-        
-        if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
-            dragging = false;
-        }
-
-        if (dragging) {
-            imagePos.x = mouse.x - dragOffset.x;
-            imagePos.y = mouse.y - dragOffset.y;
-            closeButton.x = imagePos.x + imageTexture.width - 20;
-            closeButton.y = imagePos.y;
-        }
+        ImageViewerUpdate(&viewer);
 
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        ClearBackground((Color){ 231, 229, 228, 255 });
 
-        DrawCircle(200, 200, 100, SKYBLUE);
-
-        DrawTexture(imageTexture, imagePos.x, imagePos.y, WHITE);
-        DrawRectangleRec(closeButton, RED);
-        DrawLine(closeButton.x + 4, closeButton.y + 4, closeButton.x + 16, closeButton.y + 16, WHITE);
-        DrawLine(closeButton.x + 16, closeButton.y + 4, closeButton.x + 4, closeButton.y + 16, WHITE);
+        ImageViewerRender(&viewer);
 
         EndDrawing();
     }
 
-    UnloadTexture(imageTexture);
+    ImageViewerFree(&viewer);
     CloseWindow();
 
     return 0;
